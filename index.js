4e243462e4dceb7ecaea45e7757f85d2b07a290f
Paulo Martins <paulohcmartins@hotmail.com>
2024-02-21 20:27:10 -0300

    package.json added

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const port = 4000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-  });
-  server.listen(port, hostname, () => {
+    
+    
+    switch(req.url){
+        case '/':
+            res.end("There's nothing to look for in here");
+            break;
+
+        case '/secret':
+            res.end("This is the secret path");
+            break;
+    }
+});
+
+server.listen(port, hostname, () => {
     console.log(`Hello homie, server is running at: http://${hostname}:${port}/`);
-  });
+});
+
+
