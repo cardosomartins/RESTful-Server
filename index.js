718e9d150cc3eca55191dcabf1efe539f1f570cd
Paulo Martins <paulohcmartins@hotmail.com>
2024-02-21 23:52:01 -0300

    consign pckg added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const hostname = '127.0.0.1';
 const port = 4000;
 const app = express();
+const consign = require("consign");
 
 // const server = http.createServer((req, res) => {
 //     res.statusCode = 200;
