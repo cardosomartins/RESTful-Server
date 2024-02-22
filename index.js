25eba1daf06760c57572c8d92dba92dd3cfa5ab8
Paulo Martins <paulohcmartins@hotmail.com>
2024-02-21 23:47:52 -0300

    Routes with express

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-const http = require("http");
-
+//const http = require("http");
+const express = require("express");
 const hostname = '127.0.0.1';
 const port = 4000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
     
     
-    switch(req.url){
-        case '/':
-            res.end("There's nothing to look for in here");
-            break;
+//     switch(req.url){
+//         case '/':
+//             res.end("There's nothing to look for in here");
+//             break;
 
-        case '/secret':
-            res.end("This is the secret path");
-            break;
-    }
-});
+//         case '/secret':
+//             res.end("This is the secret path");
+//             break;
+//     }
+// });
 
-server.listen(port, hostname, () => {
-    console.log(`Hello homie, server is running at: http://${hostname}:${port}/`);
-});
+app.get("/", (req, res) => {
+    res.end("There's nothing to look for in here");
+})
 
+app.get("/secret", (req, res) => {
+    res.end("Hello, you have found the secret to everything");
+})
+
+// server.listen(port, hostname, () => {
+//     console.log(`Hello homie, server is running at: http://${hostname}:${port}/`);
+// });
+
+app.listen(port, () => {
+    console.log(`Hello homie, server is running at: http://${hostname}:${port}/`);    
+})
 
