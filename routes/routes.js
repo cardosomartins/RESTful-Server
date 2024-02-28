19e242e486722d1c00b606cb6065130132c5fa3f
Paulo Martins <paulohcmartins@hotmail.com>
2024-02-27 22:47:45 -0300

    bodyParser package added

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,9 @@ module.exports = (app) => {
 
     app.get('/', (req, res) => res.send("HEy mothafucka"));
     app.get('/ameba', (req, res) => res.send("Hey amebafucka"));
+    app.post('/postTest', (req, res) => {
+        res.send(`Hey amebafucka: ${req.body}`)
+    });
 
 
 }
